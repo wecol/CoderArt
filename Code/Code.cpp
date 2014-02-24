a984277ff7a1e986936dc5da7bb471c783e59202
wecol <xunqiduan@qq.com>
2014-02-24 08:49:18 +0800

    the third commit

--- Code/Code.cpp
+++ Code/Code.cpp
@@ -2,12 +2,28 @@
 //
 
 #include "stdafx.h"
-
-
+#include "artTools.h"
+#include <iostream>
+#include <string>
+#include "SortMethods.h"
+using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int j = 11;
-	return 1;
-	int i = 0;
+	char s[9] = "abcdefgw";
+	leftShift(s, 8, 2);
+	string str = "abcdefgw";
+	rotateShift(str, 2);
+	int Array[5] = { 2, 3, 2, 4, 1 };
+	SortMethods *sorter = new SortMethods();
+	sorter->BarelSorter(Array, 5);
+	for (int i = 0; i < 5; i++)
+		printf("%d", Array[i]);
+	cout << s<<"\n" << str;
+	cout << "\n"<<(-9)%2;
+	
+	
+	
+	
+
 }
 
