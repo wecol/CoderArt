375164fec0552e8ccf9d18c521d7588751084fe8
wecol <xunqiduan@qq.com>
2014-03-05 21:03:29 +0800

    update head file

--- Code/SortMethods.h
+++ Code/SortMethods.h
@@ -4,6 +4,7 @@ class SortMethods
 public:
 	void BarelSorter(int *Array ,int length );
 	void QuickSorter(int *Array, int left, int right);
+	void CombSorter(int *Array, int length);
 	SortMethods();
 	~SortMethods();
 };
